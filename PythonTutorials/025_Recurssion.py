177d6a4b4b8dc2a94e3893bc9e89e5689b596921
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-09-05 15:01:23 +0530

    Create 025_Recurssion.py

---
+++ PythonTutorials/025_Recurssion.py
@@ -0,0 +1,12 @@
+#Recursion in python : Recursion is the process of defining something in terms of itself.
+
+def factorial(num): 
+    if (num == 1 or num == 0):
+        return 1
+    else:
+        return (num * factorial(num - 1)) 
+  
+# Driver Code
+num = 7; 
+print("Factorial: ",factorial(num))
+#Output: Factorial:  5040
